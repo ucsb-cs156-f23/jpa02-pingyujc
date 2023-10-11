e78ec48b995cbd2a9f4d583d23f72306b819e003
pingyujc <pingyujc@gmail.com>
2023-10-10 17:48:28 -0700

    changed to github id

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from ping-yujonathan!";
+        return "Greetings from pingyujc!";
     }
     
 }
